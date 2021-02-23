f8452e27b22eb580109c88acb3ef13a2a5069816
Ashish <ashishjaitiwari15112000@gmail.com>
2021-02-23 17:38:56 +0530

    removed redundant dist

--- discordbot/dist/commands/bruh.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-function default_1(args, msg) {
-    if (args[0] === "who" || args[0] === "who" || args[0] === "who") {
-        msg.channel.send("Nitya");
-    }
-}
-exports.default = default_1;
