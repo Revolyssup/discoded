aa9ae8d50fca25fcdc40eb97f70fd95191078b64
Ashish <ashishjaitiwari15112000@gmail.com>
2021-03-22 03:49:37 +0530

    added workflows

---
+++ sync.sh
@@ -0,0 +1,7 @@
+git checkout testing
+git checkout master -- server
+git checkout master -- goglot
+git add server
+git add goglot
+git commit -m "synchronized goglot and server from master."
+git push
