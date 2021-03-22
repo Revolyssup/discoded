f736d4c106c9765b8fc10968648d2de7c8a57c8c
Ashish test <ashishjaitiwari15112000@gmail.com>
2021-03-22 12:16:55 +0000

    synchronized goglot and server from master.

--- server/src/db/dao.ts
+++ server/src/db/dao.ts
@@ -4,14 +4,13 @@
  * we switch the data base in future, we only have to make changes in this class.
  */
 
-import { json } from "express";
 import { Collection, Db, MongoClient } from "mongodb";
 import redis, { RedisClient } from 'redis';
 import {promisify} from 'util'
 
 
 
-import UserDTO, { LyricDTO } from "./dto";
+import UserDTO from "./dto";
 const uri = `mongodb://root:rootpassword@mongo:27017`;
 const client = new MongoClient(uri, {
   useUnifiedTopology: true,
@@ -21,6 +20,7 @@ const redisclient:RedisClient=redis.createClient({
   host:'redis',
   port:6379,
   password:'root',
+  
 })
 
 // const getfromredis=promisify(redisclient.get).bind(client);
@@ -30,7 +30,6 @@ const redisclient:RedisClient=redis.createClient({
 export class UserDAO {
   db: Db;
   coll: Collection;
-  lyrcoll:Collection;
   cli: MongoClient;
   rcli:RedisClient;
   getfromredis:Function;
@@ -40,7 +39,6 @@ export class UserDAO {
     this.cli = client;
     this.db = this.cli.db("Ashish");
     this.coll = this.db.collection("Users");
-    this.lyrcoll=this.db.collection("Lyrics");
     this.rcli=redisClient;
     this.getfromredis=promisify(this.rcli.get).bind(this.rcli);
     this.setonredis=promisify(this.rcli.set).bind(this.rcli);
@@ -76,37 +74,7 @@ export class UserDAO {
       return JSON.parse(val);
 
   }
-
-async addLyrics(ald:LyricDTO){
-    this.rcli.set(JSON.stringify(ald.name),JSON.stringify(ald.lyrics),async (err,reply)=>{
-      this.rcli.expire(JSON.stringify(ald.name),60);
-      const prom=await this.lyrcoll.insertOne({
-          name:ald.name,
-          lyrics:ald.lyrics
-      });
-      if(!err) console.log("\nCould not cache in redis");
-      return prom;
-    })
 }
-async checkLyrics(ald:LyricDTO):Promise<string | null>{
-    const lyrics=await this.getfromredis(JSON.stringify(ald.name));
-    if(!lyrics){
-      const prom=await this.lyrcoll.find({name:ald.name}).toArray();
-        if(prom.length){
-          this.rcli.set(JSON.stringify(ald.name),JSON.stringify(prom[0].lyrics),(err,reply)=>{
-            if(err) console.log("\ncould not set cache lyrics in redis.");
-            else this.rcli.expire(JSON.stringify(ald.name),600);
-          })
-          return prom[0].lyrics;
-        }
-        return null;
-    }
-    return JSON.parse(lyrics);
-}
-
-}
-
-
 
 export default function () {
   return new Promise<UserDAO>((res, rej) => {
