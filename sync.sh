ba78ba38bc7557a823162c3524d69f83aa583a40
Ashish <ashishjaitiwari15112000@gmail.com>
2021-03-22 17:43:50 +0530

    changed sync.sh

--- sync.sh
+++ sync.sh
@@ -1,6 +1,5 @@
 git config --global user.email "ashishjaitiwari15112000@gmail.com"
 git config --global user.name "Ashish test"
-git fetch
 git checkout testing
 git checkout master -- server
 git checkout master -- goglot
