20e86085751476e819f8e7c826656b2cd140e882
Ashish <ashishjaitiwari15112000@gmail.com>
2021-04-01 00:30:56 +0530

    syncing

--- sync.sh
+++ sync.sh
@@ -4,6 +4,7 @@ git fetch
 git checkout testing
 git checkout master -- server
 git checkout master -- goglot
-git add .
+git add server 
+git add goglot 
 git commit -m "synchronized goglot and server from master."
-git push
+git push 
