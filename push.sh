3a5d82d9a8fb83bfc12eadf436640feda0e0b38d
Ashish <ashishjaitiwari15112000@gmail.com>
2021-04-01 00:40:30 +0530

    added a comment

--- push.sh
+++ push.sh
@@ -1,5 +1,4 @@
 # To sync my local testing copy with remote testing synced with master.
-git fetch
 git pull --rebase # Since master branch automatically pushes on this branch in a workflow. We need to get the latest commit from there to test in the test workflow.
 git add .
 git commit -m "$@"
