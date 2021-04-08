d39cfdc7d81071dbefac2ea75c6da90c378ab91b
Ashish test <ashishjaitiwari15112000@gmail.com>
2021-04-08 14:46:51 +0000

    synchronized goglot and server from master.

--- server/src/codehash/index.ts
+++ server/src/codehash/index.ts
@@ -1,8 +1,9 @@
 import crypto from 'crypto';
-export default function generateFilename(code:string,input:string):string{
+export default function generateFilename(code:string,input:string,lan:string):string{
     const codeplusinput={
         code,
-        input
+        input,
+        lan
     }
     // Using md5 algorithm
     return crypto.createHash('md5').update(JSON.stringify(codeplusinput)).digest("hex");
